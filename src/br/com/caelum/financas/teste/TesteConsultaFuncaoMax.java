99bb9a20971fd7221c334c382f2b05f96f78a758
andrevnl <andrevnl@hotmail.com>
2019-11-27 17:48:07 -0300

    Fazendo testes usando JPQL, criando DTO e usando NamedQueries

--- src/br/com/caelum/financas/teste/TesteConsultaFuncaoMax.java
+++ src/br/com/caelum/financas/teste/TesteConsultaFuncaoMax.java
@@ -1,26 +1,25 @@
 package br.com.caelum.financas.teste;
 
-import java.util.List;
+import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
 import br.com.caelum.financas.modelo.Conta;
-import br.com.caelum.financas.modelo.Movimentacao;
 import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
 
-public class TesteJPQL {
-
+public class TesteConsultaFuncaoMax {
+	
 	public static void main(String[] args) {
-
+		
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
 		Conta conta = new Conta();
 		conta.setId(3);
 
-		String jpql = "select m from Movimentacao m where m.conta = :pConta" 
+		String jpql = "select max(m.valor) from Movimentacao m where m.conta = :pConta" 
 				+ " and m.tipo = :pTipo"
 				+ " order by m.valor desc";
 
@@ -28,13 +27,12 @@ public class TesteJPQL {
 		query.setParameter("pConta", conta);
 		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
 
-		List<Movimentacao> resultados = query.getResultList();
-
-		for (Movimentacao movimentacao : resultados) {
-			System.out.println("Movimentação: " + movimentacao.getDescricao());
-		}
+		BigDecimal valorMaximo = (BigDecimal) query.getSingleResult();
+		
+		System.out.println("O valor maximo é: " + valorMaximo);
 
 		em.getTransaction().commit();
 		em.close();
 	}
+
 }
--- src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
+++ src/br/com/caelum/financas/teste/TesteMovimentacoesPorCategoria.java
@@ -5,28 +5,24 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import br.com.caelum.financas.modelo.Conta;
+import br.com.caelum.financas.modelo.Categoria;
 import br.com.caelum.financas.modelo.Movimentacao;
-import br.com.caelum.financas.modelo.TipoMovimentacao;
 import br.com.caelum.financas.util.JPAUtil;
 
-public class TesteJPQL {
+public class TesteMovimentacoesPorCategoria {
 
 	public static void main(String[] args) {
 
 		EntityManager em = new JPAUtil().getEntityManager();
 		em.getTransaction().begin();
 
-		Conta conta = new Conta();
-		conta.setId(3);
+		Categoria categoria = new Categoria();
+		categoria.setId(1);
 
-		String jpql = "select m from Movimentacao m where m.conta = :pConta" 
-				+ " and m.tipo = :pTipo"
-				+ " order by m.valor desc";
+		String jpql = "select m from Movimentacao m join m.categoria c where c = :pCategoria";
 
 		Query query = em.createQuery(jpql);
-		query.setParameter("pConta", conta);
-		query.setParameter("pTipo", TipoMovimentacao.SAIDA);
+		query.setParameter("pCategoria", categoria);
 
 		List<Movimentacao> resultados = query.getResultList();
 
@@ -37,4 +33,5 @@ public class TesteJPQL {
 		em.getTransaction().commit();
 		em.close();
 	}
+
 }
